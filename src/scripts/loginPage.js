142064d4240f6396e4c775988a144632d4b18b96
sdana <seth.dana@gmail.com>
2018-07-05 13:27:38 -0500

    Working login page with email and username checks

--- src/scripts/loginPage.js
+++ src/scripts/loginPage.js
@@ -5,28 +5,43 @@ const friends = require("./friends")
 const loginControl = Object.create({},{
     createForms: {
         value: function(){
-            let headline = $("<h1>Welcome to Nutshell</h1><br><h2>Please enter username to login</h2>")
+            let headline = $("<h1>Welcome to Nutshell</h1><br><h2>Please enter username and email to login</h2>")
             let userNameInput = $("<input type='text' placeholder='User Name'>")
             let emailInput = $("<input type='text' placeholder='E-Mail Address'>")
             let submitLoginBtn = $("<button>")
             submitLoginBtn.text("Submit")
-            submitLoginBtn.click(() => {loginControl.submitLogin(userNameInput.val())})
-            $("body").append(headline).append(userNameInput).append(submitLoginBtn)
-        }
-    },
-    submitLogin: {
-        value: function(userName){
-            apiController.getUserId(userName).then(user => {
-                console.log(user)
-                if (user.length === 0){
-                    alert("I'm sorry, that username is incorrect or non-existent. Please try again.")
+            submitLoginBtn.click(() => {
+                if (emailInput.val() === "" || userNameInput.val() === ""){
+                    alert("Please enter a valid username and email")
                     return
                 }
                 else {
+                    loginControl.submitLogin(userNameInput.val(), emailInput.val())
+                }
+            })
+            $("body").append(headline).append(userNameInput).append(emailInput) .append(submitLoginBtn)
+        }
+    },
+    submitLogin: {
+        value: function(userName, emailVal){
+            apiController.getUserId(userName).then(user => {
+                console.log(user)
+                if (user.length === 0){
+                    alert("I'm sorry, that username or email is incorrect or non-existent. Please try again.")
+                    return
+                }
+                else if (user[0].email === emailVal && user[0].name === userName){
                     sessionStorage.setItem("activeUser", user[0].id)
                     $("body").children().remove()
                     $("body").append($(`<h1>Welcome to Nutshell ${user[0].name}!</h1>`))
                     friends.displayFriendList()
+                }
+                else if (user[0].email !== emailVal || user[0].name !== userName) {
+                    alert("Email or username does not match")
+                    // sessionStorage.setItem("activeUser", user[0].id)
+                    // $("body").children().remove()
+                    // $("body").append($(`<h1>Welcome to Nutshell ${user[0].name}!</h1>`))
+                    // friends.displayFriendList()
                 }
             })
          }
