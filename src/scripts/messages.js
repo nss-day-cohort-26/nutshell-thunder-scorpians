48ea2055478e45b2a69fbd2a2d81f93d2acb7fe5
Philip Patton <ppatton35745@gmail.com>
2018-07-05 12:11:57 -0500

    add button for add friend and make appropriate call to friend.js

--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -2,11 +2,7 @@ const $ = require("jquery");
 const apiController = require("./apiController");
 const messagesApi = apiController["messages"];
 const friends = require("./friends");
-// console.log(apiController);
-// console.log(messagesApi);
-// console.log(messagesApi["read"]);
-// console.log(messagesApi.read)
-// const currentUser = sessionStorage.getItem("activeUser")
+
 
 let currentUser = null;
 if (sessionStorage.getItem("activeUser")) {
@@ -52,6 +48,10 @@ var buildMessagesDOM = function (messages) {
 
     // ADD MESSENGER HEADER
     messageDiv.append($("<h2>").text("Messenger"));
+    const msgOptionsInstuctions = $("<h4>").text("Use add/edit buttons to add friends and edit messages. Clicking Options will toggle buttons on and off.").attr("id", "msgOptionsInstructions")
+    msgOptionsInstuctions.hide();
+    messageDiv.append(msgOptionsInstuctions);
+
 
     // ITERATE MESSAGES
     messages.forEach(message => {
@@ -64,11 +64,13 @@ var buildMessagesDOM = function (messages) {
         const messageUserName = message["user"]["name"];
         const messageId = message["id"];
 
-        const msgItem = $("<div>").attr("id", "msg" + messageId);
-        const userSpan = $("<span>").text(`${messageUserName}`);
-        const msgSpan = $("<span>").text(`: ${messageText}`).addClass("msgSpan");
+        const msgItem = $("<div>").attr("id", messageId);
+        const userSpan = $("<span>").text(`${messageUserName}`).addClass("user");
+        const seperatorSpan = $("<span>").text(": ");
+        const msgSpan = $("<span>").text(`${messageText}`).addClass("msgSpan");
 
         msgItem.append(userSpan);
+        msgItem.append(seperatorSpan);
         msgItem.append(msgSpan);
 
         //IF MESSAGE BELONGS TO CURRENT USER, ALLOW TO CONFIGURE
@@ -110,11 +112,13 @@ var buildMessagesDOM = function (messages) {
 
             // TOGGLE EDIT OPTIONS
             editButton.on("click", (e) => {
-                console.log(msgItem.children(".msgSpan").textContent);
+                // console.log(msgItem.children(".msgSpan").text());
+                const curMessage = msgItem.children(".msgSpan").text();
+
                 msgItem.children(".msgSpan").hide();
                 e.target.style.display = "none";
-                editInput.attr("value", msgItem.children(".msgSpan").textContent);
                 editInput.show();
+                editInput.val(curMessage);
                 saveButton.show();
                 cancelButton.show();
                 deleteButton.show();
@@ -122,10 +126,17 @@ var buildMessagesDOM = function (messages) {
 
             msgItem.append(editButton).append(editInput).append(saveButton).append(cancelButton).append(deleteButton);
         } else {
-            userSpan.on("click", (e) => {
-                // alert(e.target.textContent);
-                friends.addFriend(e.target.textContent);
+
+            const addFriendButton = $("<button>").text("Add Friend").addClass("addFriendButton").on("click", (e) => {
+
+                const pd = $(e.target).parent();
+                const us = pd.children(".user");
+                // console.log(us.text());
+                freinds.addFriend(us.text())
             });
+            addFriendButton.hide()
+            msgItem.append(addFriendButton);
+
         }
 
         //APPEND MESSAGE ITEM
@@ -135,17 +146,31 @@ var buildMessagesDOM = function (messages) {
     // CREATE NEW MESSAGE AREA AT BOTTOM OF BOX
     const newMessageDiv = $("<div>");
     const newMessageInput = $("<input>").attr("id", "newMessageInput");
-    const newMessageButton = $("<button>").text("submit").attr("id", "newMessageSubmitButton");
-    const msgOptionButton = $("<button>").text("Options").attr("id", "msgOptionButton");
+
+    const newMessageButton = $("<button>").text("Send").attr("id", "newMessageSubmitButton");
     newMessageButton.on("click", (e) => {
         const message = $("#newMessageInput").val()
         Messages.create(currentUser, message)
     })
+
+    const msgOptionButton = $("<button>").text("Options").attr("id", "msgOptionButton");
     msgOptionButton.on("click", (e) => {
+        if ($("#msgOptionsInstuctions").is(":visible")) {
+            $("#msgOptionsInstructions").hide();
+        } else {
+            $("#msgOptionsInstructions").show();
+        }
+
         if ($(".editMsgButton").is(":visible")) {
             $(".editMsgButton").hide();
         } else {
             $(".editMsgButton").show();
+        }
+
+        if ($(".addFriendButton").is(":visible")) {
+            $(".addFriendButton").hide();
+        } else {
+            $(".addFriendButton").show();
         }
 
     })
