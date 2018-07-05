477dbe6931edd9b6e7bcd508e734dba14f133173
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-05 15:29:03 -0500

    Created event-article to load the event section header and add new event button

--- src/scripts/Events/events.js
+++ src/scripts/Events/events.js
@@ -1,31 +1,14 @@
 
 const $ = require("jquery");
 const apiController = require("../apiController");
+const loadEventArticle = require("./event-article");
 const loadEvents = require("./event-load");
-const createNewEvent = require("./event-form");
 
 const events = () => {
 
-console.log("events page is working");
-
-/* Page Has Loaded:
-1. Add New Event Button
-*/
-
-const $eventHeader = $("<h2>").attr("id", "event-header")
-$eventHeader.text("Events");
-$(".events").append($eventHeader);
-
-const $newEventButton = $("<button>").attr("id", "add-event");
-$newEventButton.text("Add Event").click(createNewEvent);
-$(".events").append($newEventButton);
-
-const $eventArticle = $("<article>").attr("id", "event-article");
-$(".events").append($eventArticle);
-
+console.log("Events is running");
+loadEventArticle();
 loadEvents();
-
-//createNewEvent();
 
 }
 
