f40ae03f5a13ae43023d8b45ace6d060338ffeea
sdana <seth.dana@gmail.com>
2018-07-05 09:52:05 -0500

    changing add friend functionality to work with messages, and build dom components seperately, commiting before changing a bunch of code

--- src/scripts/friends.js
+++ src/scripts/friends.js
@@ -24,15 +24,27 @@ const friendActions = Object.create({},{
             })
         }
     },
-    addFriend: {
+    makeDomComponents: {
         value: function(){
             const friendNameInput = $("<input type='text' placeholder='Enter Friend Name'></input>")
             const saveButton = $("<button>")
             saveButton.text("Save Friend")
             friendsList.append(saveButton).append(friendNameInput)
+            saveButton.click(() => {friendName = friendNameInput.val(); friendActions.addFriend(friendName)})
+        }
+    },
+    addFriend: {
+        value: function(){
+            // const friendNameInput = $("<input type='text' placeholder='Enter Friend Name'></input>")
+            // const saveButton = $("<button>")
+            // saveButton.text("Save Friend")
+            // friendsList.append(saveButton).append(friendNameInput)
             let friendsToCheck = []
             apiController.getFriendsList(currentUser).then( (response) =>{friendsToCheck = response})
-            saveButton.click(() => {
+            // saveButton.click(() => {
+                // console.log("friends to check", friendsToCheck)
+                friendNameArray = friendsToCheck.map((currentValue, index) =>{return friendsToCheck[index].user.name})
+                // console.log(friendNameArray)
                 if (friendNameInput.val() === ""){
                     alert("Please enter a valid username")
                     saveButton.remove()
@@ -40,7 +52,15 @@ const friendActions = Object.create({},{
                     addFriendBtn.show()
                     return
                 }
+                else if (friendNameArray.includes(friendNameInput.val())){
+                    alert(`You're already friends with ${friendNameInput.val()}`)
+                    saveButton.remove()
+                    friendNameInput.remove()
+                    addFriendBtn.show()
+                    return
+                }
                 else {apiController.getUserId(friendNameInput.val()).then(response => {
+                    console.log("getUserId Response", response[0].id)
                     if (response.length === 0){
                         alert(`I'm sorry, user ${friendNameInput.val()} doesn't exist`)
                         friendNameInput.remove()
@@ -48,6 +68,13 @@ const friendActions = Object.create({},{
                         friendActions.displayFriendList()
                         // addFriendBtn.show()
                         }
+                    else if (String(response[0].id) === currentUser){
+                        alert("You cannot add yourself as a friend, friend.")
+                        saveButton.remove()
+                        friendNameInput.remove()
+                        addFriendBtn.show()
+                        return
+                    }
                     else {
                         apiController.addNewFriend(currentUser, response[0].id)
                         friendNameInput.remove()
@@ -57,12 +84,13 @@ const friendActions = Object.create({},{
                         }
                     })
                 }
-            })
+            // })
         }
     }
 })
 
-addFriendBtn.click(() => { addFriendBtn.hide(); friendActions.addFriend() })
+// addFriendBtn.click(() => { addFriendBtn.hide(); friendActions.addFriend() })
+addFriendBtn.click(() => { addFriendBtn.hide(); friendActions.makeDomComponents() })
 
 friendActions.displayFriendList()
 
