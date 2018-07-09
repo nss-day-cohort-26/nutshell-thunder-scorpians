5c6702427722200efa94d57a0663994db954828c
Philip Patton <ppatton35745@gmail.com>
2018-07-09 10:34:59 -0500

    changed message colors from black to blue

--- src/scripts/messages.js
+++ src/scripts/messages.js
@@ -6,7 +6,7 @@ const apiController = require("./apiController");
 const messagesApi = apiController["messages"];
 const friends = require("./friends");
 
-sessionStorage.setItem("messageChange", 1);
+localStorage.setItem("messageChange", 1);
 
 // API
 const Messages = {
@@ -34,8 +34,8 @@ const Messages = {
         const curTimeStamp = new Date();
         messagesApi.create(userId, message, curTimeStamp).then(() => {
             this.read("createNew");
-            let sS = sessionStorage.getItem("messageChange");
-            sessionStorage.setItem("messageChange", ++sS);
+            let sS = localStorage.getItem("messageChange");
+            localStorage.setItem("messageChange", ++sS);
         })
     },
 
@@ -44,8 +44,8 @@ const Messages = {
 
         messagesApi.update(msgId, userId, newMessage, messageTimeStamp).then(() => {
             this.read();
-            let sS = sessionStorage.getItem("messageChange");
-            sessionStorage.setItem("messageChange", ++sS);
+            let sS = localStorage.getItem("messageChange");
+            localStorage.setItem("messageChange", ++sS);
         })
 
     },
@@ -239,17 +239,17 @@ var buildMessagesDOM = function (messages, currentUser) {
 
 // $(window).on("storage", function (e) {
 //     alert("eventFired");
-//     if (e.originalEvent.storageArea === sessionStorage) {
+//     if (e.originalEvent.storageArea === this.localStorage) {
 //         alert("change1");
 //     }
 //     // else, event is caused by an update to localStorage, ignore it
 // });
 
-// window.addEventListener("storage", function(storageEvent){
-//     // the event seems not to fire on own state changes, only other windows
-//     console.log(storageEvent);
-//     App.controller.setLoginState(storageEvent.newValue);
-// }, false);
+window.addEventListener("storage", function(storageEvent){
+    // the event seems not to fire on own state changes, only other windows
+    console.log(storageEvent);
+    alert("changed");
+}, false);
 
 // $(window).bind("storage", function (e) {
 //     alert("change2");
